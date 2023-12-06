6a637f56e7700b2070565ba99a4e09cac16576fb
Denis <unrealgoodandyou@mail.ru>
2023-12-06 17:15:06 +0300

    removed extra code

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "./test/index";
+import counterReducer from "./counterReducer/index";
 import modalOrder from "../components/Modal/Order/store/index";
 import mainPage from "../pages/Main/store/index";
 
